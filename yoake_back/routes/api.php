ec68beb981c68968c0a02f93190cd352d91d4787
Your Name <mailsonccardoso@gmail.com>
2026-02-14 19:44:53 -0300

    fix: add name to login route to resolve RouteNotFoundException

--- yoake_back/routes/api.php
+++ yoake_back/routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Auth
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
